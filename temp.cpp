f72a47279b8dfb276d7eeb30c9efd9f219e12f94
neel0502 <77948821+neel0502@users.noreply.github.com>
2023-02-17 08:39:10 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World This is N P NEELESH\n";
+  cout << "THIS IS MY Task5 my SRN is PES1UG20CS258\n";
+  return 0;
+}
